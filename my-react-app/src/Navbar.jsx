2f67f5eace96ff9434db1098b3cd53c1ac1cf2f4
Mark Villanueva <markchristianvillanueva23@gmail.com>
2026-08-19 08:16:01 +0800

    feat: add my own logo picture

--- my-react-app/src/Navbar.jsx
+++ my-react-app/src/Navbar.jsx
@@ -1,7 +1,27 @@
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/278110208?s=400&u=7caed1f09f709f5317a196919d6f6a1916dceea0&v=4"
+                        alt="My Avatar"
+                        width="36"
+                        height="36"
+                        style={{ borderRadius: "50%", objectFit: "cover" }}
+                    />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
