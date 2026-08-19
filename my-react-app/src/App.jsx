05f2b0d0c59cb477e934bc863876e94be7bb5588
Mark Villanueva <markchristianvillanueva23@gmail.com>
2026-08-19 08:11:09 +0800

    feat: removed unused forms, change navbars to a portfolio style

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar.jsx";
 import Sidebar from "./Sidebar.jsx";
-import Forms from "./Forms.jsx";
 import Footer from "./Footer.jsx";
 
 function App() {
@@ -13,10 +12,9 @@ function App() {
 
                 <main className="p-4 flex-grow-1">
                     <h1 className="mb-4">Welcome</h1>
-                    <Forms />
                 </main>
-            </div>
 
+            </div>
             <Footer />
         </>
     );
